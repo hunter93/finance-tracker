702190261ad8633c230b1f57f4fd2255d5ac6135
Hunter Ogden <hunter.ogden@workwave.com>
2023-01-27 19:56:28 -0500

    attempting to fix issue with ActionController::unknownFormat

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,7 +5,11 @@ class Stock < ApplicationRecord
       publishable_token: Rails.application.credentials.iex_client[:public_key],
       endpoint: 'https://finance-tracker-1993.iex.cloud/v1/'
     )
-    new(ticker: ticker_symbol, name: client.quote(ticker_symbol).company_name, last_price: client.quote(ticker_symbol).latest_price)
+    begin
+      new(ticker: ticker_symbol, name: client.quote(ticker_symbol).company_name, last_price: client.quote(ticker_symbol).latest_price)
+    rescue => exception
+      return nil
+    end
   end
 end
 
