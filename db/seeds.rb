83c117853c2868a1188036b5d32cd289daf2efbe
Hunter Ogden <hunter.ogden@workwave.com>
2023-01-28 05:43:13 -0500

    adding validations, model, controller, seeding bd; basically making all the connections between users and stocks to set up for putting them out on the page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "hogden2@email.com", password: "password")
+User.create(email: "hogden3@email.com", password: "password")
+User.create(email: "hogden4@email.com", password: "password")
+User.create(email: "hogden5@email.com", password: "password")
+User.create(email: "hogden6@email.com", password: "password")
+User.create(email: "hogden7@email.com", password: "password")
